3d58c7ee4f42ea3559008aede8382ed5e9971459
Jacobo D'león <jacobodleon@gmail.com>
2026-09-03 23:18:34 -0500

    r require zero frontend lint errors

--- verify.ps1
+++ verify.ps1
@@ -1,4 +1,4 @@
-param([int]$MaxLintErrors = 7, [int]$MaxLintWarnings = 8)
+param([int]$MaxLintErrors = 0, [int]$MaxLintWarnings = 8)
 
 $ErrorActionPreference = 'Stop'
 $taskPython = Join-Path $PSScriptRoot '.venv/Scripts/python.exe'
